e18f68044ed4cc8300beee83bef61c6be302ecf0
Narawish Sangsiriwut <narawish.s@ku.th>
2021-12-01 23:06:51 +0700

    get movie rating

--- get_rating.py
+++ get_rating.py
@@ -1,3 +1,5 @@
+import csv
+
 import requests
 
 BASE_URL = "https://imdb-api.com/en"
@@ -5,29 +7,49 @@ API_KEY = ["k_zwp5i4yu",
            "k_p7h75hc0",
            "k_pu9wbz8u",
            "k_q514guzp",
-           "k_2uzksic3",
-           "k_21f13b32",
-           "k_33j1rc5f",
-           "k_z218xks1",
-           "k_jme3zq72",
-           "k_x8130z3o",
-           "k_0fquph46"]
+           "k_2uzksic3"]
+
+
+# "k_21f13b32",
+# "k_33j1rc5f",
+# "k_z218xks1",
+# "k_jme3zq72",
+# "k_x8130z3o",
+# "k_0fquph46"]
 
 
 def get_movie_rating(imdb_id):
-    response = requests.get(f"{BASE_URL}/API/Ratings/{API_KEY[0]}/{imdb_id}").json()
+    for API in API_KEY:
+        response = requests.get(f"{BASE_URL}/API/Ratings/{API}/{imdb_id}").json()
+        if response['errorMessage'] == '':
+            break
+    imDb = response['imDb']
+    metacritic = response['metacritic']
+    theMovieDb = response['theMovieDb']
+    if imDb == '':
+        imDb = 0
+    if metacritic == '':
+        metacritic = 0
+    if theMovieDb == '':
+        theMovieDb = 0
     return {
         "imdb_id": imdb_id,
         "title": response['title'],
-        "imDb": response['imDb'],
-        "metacritic": response['metacritic'],
-        "theMovieDb": response['theMovieDb'],
-        "rottenTomatoes": response['rottenTomatoes'],
-        "tV_com": response['tV_com'],
-        "filmAffinity": response['filmAffinity'],
-        "errorMessage": response['errorMessage']
+        "imDb": imDb,
+        "metacritic": metacritic,
+        "theMovieDb": theMovieDb
     }
 
 
 if __name__ == '__main__':
-    print(get_movie_rating("tt1477834"))
+    keys = ['imdb_id', 'title', 'imDb', 'metacritic', 'theMovieDb']
+    with open("movie_imdb.csv", encoding='utf-8') as file:
+        data_list = file.readlines()
+        with open('movie_rating.csv', 'w', encoding='utf-8', newline="") as my_file:
+            writer = csv.DictWriter(my_file, keys)
+            i = 1
+            for data in data_list:
+                result = get_movie_rating(data.strip('\n'))
+                writer.writerow(result)
+                print(i, result)
+                i += 1
